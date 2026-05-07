eb38ddeba9258478d2540f320c502cf63aef5952
dhia <medhioub.dhia@gmail.com>
2026-05-07 17:14:34 +0300

    test full gitops flow v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevSecOps Pipeline is Running 🚀"
+    return "Hello from version 2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
